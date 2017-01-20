f8469e07243e84489a92c5e8bbde124c9bb54482
Jean BOSSUET-TIERCELIN <jebossue@e3r9p17.42.fr>
2017-01-20 12:24:41 +0100

    invalide fd done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jebossue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 15:08:41 by jebossue          #+#    #+#             */
-/*   Updated: 2017/01/11 19:36:09 by jebossue         ###   ########.fr       */
+/*   Updated: 2017/01/20 12:24:19 by jebossue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,8 +58,10 @@ int		get_next_line(const int fd, char **line)
 {
 	static t_struct	stru = {.slash = -1, .ret = 0, .i = 0};
 
+	if (line == NULL || fd < 0)
+		return (-1);
 	*line = NULL;
-	if (stru.ret != 0)
+	if (stru.ret > 0)
 	{
 		if ((ft_slash_n(line, &stru)) == 1)
 			return (1);
@@ -72,12 +74,12 @@ int		get_next_line(const int fd, char **line)
 			return (1);
 		stru.i = stru.i + stru.ret;
 	}
+	if (stru.ret < 0)
+		return (-1);
 	if (stru.ret == 0 && stru.i > 0)
 	{
 		stru.i = 0;
 		return (1);
 	}
-	if (stru.ret == 0)
-		return (0);
-	return (-1);
+	return (0);
 }
